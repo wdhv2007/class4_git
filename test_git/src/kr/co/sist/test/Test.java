713195880fd3c7ad50ab8a6de9a1fddafb24bebe
user <user@user-PC>
2017-05-23 11:41:20 +0900

    두번째 마스터 수정-우동하

--- test_git/src/kr/co/sist/test/Test.java
+++ test_git/src/kr/co/sist/test/Test.java
@@ -3,8 +3,8 @@ package kr.co.sist.test;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i=0;
+		System.out.println("º¯¼ö °ª i="+i);
 	}
 
 }
